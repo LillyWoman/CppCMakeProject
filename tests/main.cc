57ce2af7008119431202cbfffb10e2095805c809
Simone Puschmann <simone-puschmann@t-online.de>
2021-02-10 00:32:13 +0100

    Anpassungen für CI

--- tests/main.cc
+++ tests/main.cc
@@ -1,11 +1,26 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 
 #include "catch2/catch.hpp"
 
 #include "my_lib.h"
 
+TEST_CASE("Test my cout function", "TestCout")
+{
+    cout_hello_world();
+}
 
-TEST_CASE( "Boost version is testing", "[BoostTest]" ) {
-    int ver = print_boost_version();
-    REQUIRE( ver == 107500);
+TEST_CASE("Test my vector function", "TestVector")
+{
+    linalg::aliases::float3 v = print_linalg_vector();
+
+    REQUIRE(1 == v[0]);
+    REQUIRE(2 == v[1]);
+    REQUIRE(3 == v[2]);
+}
+
+TEST_CASE("Test my boost function", "TestBoost")
+{
+    bool b = print_boost_version();
+
+    REQUIRE(true == b);
 }
